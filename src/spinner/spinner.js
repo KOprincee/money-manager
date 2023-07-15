9b8e47fdc146437cd473fab858563c6190343b15
Prince Kothari <princemkothari@gmail.com>
2023-07-15 23:17:31 +0530

    creating spinner for loading screen

---
+++ src/spinner/spinner.js
@@ -0,0 +1,14 @@
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <>
+      <div className="background-div"></div>
+      <div className="spinner-container">
+        <div className="cp-spinner cp-meter"></div>
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
